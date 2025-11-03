62918f654520181a88dceb5059880e6e92d215ca
depstor <depstor90@gmail.com>
2025-11-03 15:15:26 +0300

    t

--- src/js/api.js
+++ src/js/api.js
@@ -1,15 +1,17 @@
 const API_KEY = 'MFFQ01LPDIUI4H4S';
 
 export async function getCryptoPrice(cryptoSymbol) {
-    const apiDataResponse = document.querySelector('.apiResponse')
-    apiDataResponse.innerHTML = `🔹 <span class="text-sm text-gray-500">Запрашиваем данные для:</span> <span class="text-sm text-yellow-500">${cryptoSymbol}</span>`
-
+    const apiDataResponse = document.querySelectorAll('.apiResponse');
+    apiDataResponse.forEach(res => {
+        res.innerHTML = `⏳ <span class="text-sm text-gray-500">Запрашиваем данные для:</span> <span class="text-sm text-yellow-500">${cryptoSymbol}</span>`;
+    });
     try {
         const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${cryptoSymbol}&to_currency=USD&apikey=${API_KEY}`;
 
         const response = await fetch(url);
-        console.log(' Статус ответа:', response.status);
-        apiDataResponse.innerHTML = `✔️ <span class="text-sm text-gray-500">Статус ответа:</span> <span class="text-sm text-green-500">${response.status}</span>`
+        apiDataResponse.forEach(res => {
+            res.innerHTML = `✔️ <span class="text-sm text-gray-500">Статус ответа:</span> <span class="text-sm text-green-500">${response.status}</span>`;
+        })
 
         const data = await response.json();
         console.log(' Данные от API:', data);
@@ -17,7 +19,9 @@ export async function getCryptoPrice(cryptoSymbol) {
         return data;
 
     } catch (error) {
-        apiDataResponse.innerHTML = `🔴 <span class="text-sm text-red-500">Ошибка: ${error.message}</span>  `
+        apiDataResponse.forEach(res => {
+            res.innerHTML = `🔴 <span class="text-sm text-red-500">Ошибка: ${error.message}</span>  `;
+        });
         return null;
-    }
-}
+    };
+};
